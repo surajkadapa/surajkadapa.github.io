0b05c377cb5a5a4c429e62b4701bba823db714aa
Suraj Kadapa <surajkadapa103@gmail.com>
2025-03-10 20:02:03 +0530

    Fix GitHub Pages deployment

--- astro.config.ts
+++ astro.config.ts
@@ -23,7 +23,8 @@ import rehypeUnwrapImages from "rehype-unwrap-images";
 
 // https://astro.build/config
 export default defineConfig({
-	site: siteConfig.url,
+	site: "https://surajkadapa.github.io",
+	base: "/",
 	image: {
 		domains: ["webmention.io"],
 	},

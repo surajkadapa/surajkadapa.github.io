b40b8d1a1c49f6879019ff5360606ac7b55dc0b3
Suraj Kadapa <surajkadapa103@gmail.com>
2025-03-11 22:34:43 +0530

    social card fixingv2

--- src/site.config.ts
+++ src/site.config.ts
@@ -14,7 +14,7 @@ export const siteConfig: SiteConfig = {
 		},
 	},
 	// Used as the default description meta property and webmanifest description
-	description: "An opinionated starter theme for Astro",
+	description: "A blog about low level computing and other fun stuff",
 	// HTML lang property, found in src/layouts/Base.astro L:18 & astro.config.ts L:48
 	lang: "en-GB",
 	// Meta property, found in src/components/BaseHead.astro L:42
